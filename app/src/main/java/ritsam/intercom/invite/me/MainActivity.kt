fa29b45f86979d68fc831be627a168fd0aa0b670
RitSam <git.rituraj.sambherao@gmail.com>
2021-05-23 13:41:34 +0100

    Initial Commit

---
+++ app/src/main/java/ritsam/intercom/invite/me/MainActivity.kt
@@ -0,0 +1,18 @@
+package ritsam.intercom.invite.me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ritsam.intercom.invite.me.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ritsam/intercom/invite/me/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ritsam.intercom.invite.me.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
